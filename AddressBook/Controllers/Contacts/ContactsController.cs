a3434e2c9710a94999c017a03f634b50a397a8e9
Nova Wiffen <nova.wiffen@gmail.com>
2019-12-05 12:30:02 +0200

    Added business logic layer

---
+++ AddressBook/Controllers/Contacts/ContactsController.cs
@@ -0,0 +1,101 @@
+﻿using AddressBook.Models;
+using AddressBookEntityFramework;
+using AddressBookEntityFramework.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AddressBook.Contacts.Controllers
+{
+    public class ContactsController : IContactsController
+    {
+        public Models.Contact CreateContact(ContactModel createRequest)
+        {
+            var contactEntity = ConvertContactModelToEntity(createRequest);
+            using (var dbContext = new ContactsContext())
+            {
+                dbContext.Contacts.Add(contactEntity);
+                dbContext.SaveChanges();
+            }
+            return ConvertContactEntityToContactModel(contactEntity);
+        }
+
+        public bool DeleteContact(Guid ContactId)
+        {
+            using (var dbContext = new ContactsContext())
+            {
+                var searchResults = dbContext.Contacts.Where(x => x.ExternalId == ContactId);
+                if (searchResults.Count() == 0) { 
+                    return false;
+                }
+
+                var entity = searchResults.First();
+                dbContext.Contacts.Remove(entity);
+                dbContext.SaveChanges();
+            }
+            return true;
+        }
+
+        public IEnumerable<Models.Contact> GetContacts()
+        {
+            using (var dbContext = new ContactsContext())
+            {
+                var contacts = dbContext.Contacts.Select(x => ConvertContactEntityToContactModel(x));
+                return contacts;
+            }
+        }
+
+        public IEnumerable<Models.Contact> SearchContacts(string searchTerm)
+        {
+            using (var dbContext = new ContactsContext())
+            {
+                var searchResults = dbContext.Contacts.Where(
+                    x => x.FirstName.Contains(searchTerm)
+                    || x.LastName.Contains(searchTerm)
+                );
+                return searchResults.Select(x => ConvertContactEntityToContactModel(x));
+            }
+        }
+
+        public Models.Contact UpdateContact(Models.Contact updateRequest)
+        {
+            using (var dbContext = new ContactsContext())
+            {
+                var contactToUpdate = dbContext.Contacts.Where(x => x.ExternalId == updateRequest.ExternalId).First();
+                contactToUpdate.FirstName = updateRequest.ContactModel.FirstName;
+                contactToUpdate.LastName = updateRequest.ContactModel.LastName;
+                contactToUpdate.ContactNumbers = updateRequest.ContactModel.ContactNumbers.Select(x => new ContactNumber { Number = x }).ToList();
+                contactToUpdate.EmailAddresses = updateRequest.ContactModel.EmailAddresses.Select(x => new EmailAddress { Address = x }).ToList();
+                dbContext.SaveChanges();
+                return ConvertContactEntityToContactModel(contactToUpdate);
+            }
+        }
+
+        private AddressBookEntityFramework.Models.Contact ConvertContactModelToEntity(ContactModel contactModel)
+        {
+            return new AddressBookEntityFramework.Models.Contact
+            {
+                FirstName = contactModel.FirstName,
+                LastName = contactModel.LastName,
+                ContactNumbers = contactModel.ContactNumbers.Select(x => new ContactNumber { Number = x }).ToList(),
+                EmailAddresses = contactModel.EmailAddresses.Select(x => new EmailAddress { Address = x }).ToList()
+            };
+
+        }
+        private Models.Contact ConvertContactEntityToContactModel(AddressBookEntityFramework.Models.Contact entity)
+        {
+            return new Models.Contact
+            {
+                ExternalId = entity.ExternalId,
+                ContactModel = new ContactModel
+                {
+                    FirstName = entity.FirstName,
+                    LastName = entity.LastName,
+                    ContactNumbers = entity.ContactNumbers.Select(x => x.Number).ToList(),
+                    EmailAddresses = entity.EmailAddresses.Select(x => x.Address).ToList()
+                }
+            };
+        }
+
+    }
+}
