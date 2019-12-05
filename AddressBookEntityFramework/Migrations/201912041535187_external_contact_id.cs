7708f777f3238afa07604c2065a4e31ed147ae26
Nova Wiffen <nova.wiffen@gmail.com>
2019-12-05 12:25:42 +0200

    Added external ID to contact model in DB

---
+++ AddressBookEntityFramework/Migrations/201912041535187_external_contact_id.cs
@@ -0,0 +1,18 @@
+﻿namespace AddressBookEntityFramework.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class external_contact_id : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Contact", "ExternalId", c => c.Guid(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Contact", "ExternalId");
+        }
+    }
+}
